a0ed11101291e208a602244baf6150c915de79dc
VishvaH47 <vishvah.nexitence@gmail.com>
2023-12-21 16:09:03 +0530

    new file was updated

--- AttendanceCalendar.java
+++ AttendanceCalendar.java
@@ -1,19 +1,48 @@
-package com.code.glancer.interview.schedule.dto;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-import b1weekly.property.Attendence;
-import b1weekly.property.Organizer;
-import lombok.Data;
+public class AttendanceCalendar {
 
-import java.time.LocalDateTime;
-import java.util.List;
-public class calendar
-{
-    private 
-    private
-    private
-    private
-    private
-    private
-    private
+    public static void main(String[] args) {
+        Map<String, Map<String, Boolean>> attendanceData = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter username (type 'exit' to quit): ");
+            String username = scanner.nextLine();
+
+            if (username.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            // Initialize user's attendance map if not present
+            attendanceData.putIfAbsent(username, new HashMap<>());
+
+            System.out.println("Enter date (YYYY-MM-DD): ");
+            String date = scanner.nextLine();
+
+            System.out.println("Is " + username + " present on " + date + "? (yes/no): ");
+            String attendanceInput = scanner.nextLine().toLowerCase();
+
+            // Mark attendance based on user input
+            boolean isPresent = attendanceInput.equals("yes");
+            attendanceData.get(username).put(date, isPresent);
+
+            System.out.println("Attendance marked for " + username + " on " + date);
+        }
+
+        // Display attendance data
+        System.out.println("Attendance Data:");
+        for (Map.Entry<String, Map<String, Boolean>> entry : attendanceData.entrySet()) {
+            String username = entry.getKey();
+            Map<String, Boolean> userAttendance = entry.getValue();
+
+            System.out.println(username + ": " + userAttendance);
+        }
+    }
+}
+
 
 }
