59127a9168b5c5ef33191602afe3eb2fc82594ca
VishvaH47 <vishvah.nexitence@gmail.com>
2023-12-21 16:05:33 +0530

    new name was changed

---
+++ AttendanceCalendar.java
@@ -0,0 +1,19 @@
+package com.code.glancer.interview.schedule.dto;
+
+import b1weekly.property.Attendence;
+import b1weekly.property.Organizer;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+public class calendar
+{
+    private 
+    private
+    private
+    private
+    private
+    private
+    private
+
+}
